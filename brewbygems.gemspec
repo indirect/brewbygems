81e552b85067175ae71d966451ac75fe524d6313
Andre Arko <andre@arko.net>
2010-11-05 00:29:53 -0700

    Massive upgrade, biggest change is that there is no output by default
    - Add Brewbygems module
    - Don't double-load the plugin if multiple versions are installed
    - Only allow the brew command to print stdout when $VERBOSE is set

--- brewbygems.gemspec
+++ brewbygems.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{brewbygems}
-  s.version = "0.3.1"
+  s.version = "0.4.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Andre Arko"]
-  s.date = %q{2010-01-26}
+  s.date = %q{2010-11-05}
   s.description = %q{Adds RubyGems post-install and post-uninstall hooks to update Homebrew bin/ symlinks}
   s.email = %q{andre@arko.net}
   s.extra_rdoc_files = [
@@ -26,14 +26,14 @@ Gem::Specification.new do |s|
   s.homepage = %q{http://github.com/indirect/brewbygems}
   s.rdoc_options = ["--charset=UTF-8"]
   s.require_paths = ["lib"]
-  s.rubygems_version = %q{1.3.5}
+  s.rubygems_version = %q{1.3.7}
   s.summary = %q{Makes sure RubyGems plays nice with Homebrew}
 
   if s.respond_to? :specification_version then
     current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
     s.specification_version = 3
 
-    if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
+    if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
     else
     end
   else
--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,21 +1,49 @@
-Gem::post_install do |installer|
-  cellar = File.join(`brew --prefix`.chomp, "Cellar")
-  # Augh RubyGems why is installer.gem_home a Pathname here
-  installing_to_cellar = installer.gem_home.to_s.include?(cellar)
-  has_binary = installer.spec.executables.any?
+if !defined?(Brewbygems::VERSION) || Gem::Version.new(Brewbygems::VERSION) < Gem::Version.new("0.3.0")
+  module Brewbygems
+    VERSION = "0.3.0"
 
-  unless system("brew link gems")
-    puts "brewbygems: Sorry, you don't seem to have Homebrew installed."
-  end if has_binary && installing_to_cellar
-end
+    class << self
 
-Gem::post_uninstall do |installer|
-  cellar = File.join(`brew --prefix`.chomp, "Cellar")
-  # Augh RubyGems why is installer.gem_home a String here
-  installing_to_cellar = installer.gem_home.to_s.include?(cellar)
-  has_binary = installer.spec.executables.any?
+      def prune
+        puts "brewbygems: running command `brew prune #{silence}`" if $VERBOSE
+        system("brew prune #{silence}")
+      end
 
-  unless system("brew prune")
-    puts "brewbygems: Sorry, you don't seem to have Homebrew installed."
-  end if has_binary && installing_to_cellar
+      def link
+        puts "brewbygems: running command `brew link gems #{silence}`" if $VERBOSE
+        system("brew link gems #{silence}")
+      end
+
+    private
+
+      def silence
+        $VERBOSE ? "" : "> /dev/null"
+      end
+
+    end
+  end
+
+  Gem::post_install do |installer|
+    cellar = File.join(`brew --prefix`.chomp, "Cellar")
+    # Augh RubyGems why is installer.gem_home a String here
+    installing_to_cellar = installer.gem_home.to_s.include?(cellar)
+    has_binary = installer.spec.executables.any?
+
+    if has_binary && installing_to_cellar
+      success = Brewbygems.link
+      puts "brewbygems: Sorry, you don't seem to have Homebrew installed." unless success
+    end
+  end
+
+  Gem::post_uninstall do |installer|
+    cellar = File.join(`brew --prefix`.chomp, "Cellar")
+    # Augh RubyGems why is installer.gem_home a String here
+    installing_to_cellar = installer.gem_home.to_s.include?(cellar)
+    has_binary = installer.spec.executables.any?
+
+    if has_binary && installing_to_cellar
+      success = Brewbygems.prune
+      puts "brewbygems: Sorry, you don't seem to have Homebrew installed." unless success
+    end
+  end
 end
