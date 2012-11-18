3f0055670c0fcefc171834e61fb0db179586957e
kevin birch <kmb@pobox.com>
2012-11-18 16:20:52 -0500

    add support for sites that use Ruby from Homebrew, linking executables from the Homebrew bin directory to Ruby cellar; update link behavior for gems formula to support recent Homebrew

--- lib/brewbygems.rb
+++ lib/brewbygems.rb
@@ -2,9 +2,18 @@ module Brewbygems
   class << self
     attr_accessor :loaded_version
 
-    def link
-      puts "brewbygems: running command `brew link gems`" if Gem.configuration.verbose == 1
-      system("brew link gems #{silence}")
+    def link(installer)
+      if system("brew ls gems > /dev/null")
+        puts "brewbygems: relinking gems formula" if Gem.configuration.verbose == 1
+        system("brew unlink #{silence} gems && brew link gems #{silence}")
+      elsif system("ls `brew --prefix`/bin/ruby")
+        installer.spec.executables.each { |each|
+          prefix = `brew --prefix`.chomp
+          puts "brewbygems: linking executable `#{each}' into #{prefix}/bin" if Gem.configuration.verbose == 1
+          ruby_target = `readlink #{prefix}/bin/ruby`.chomp
+          system("cd #{prefix}/bin; ln -s `dirname #{ruby_target}`/#{each} #{silence}")
+        }
+      end
     end
 
     def prune
@@ -28,7 +37,7 @@ Gem::post_install do |installer|
   has_binary = installer.spec.executables.any?
 
   if has_binary && installing_to_cellar
-    success = Brewbygems.link
+    success = Brewbygems.link(installer)
     puts "brewbygems: Sorry, you don't seem to have Homebrew installed." unless success
   end
 end
